1dfb5bc664bb37265c9f0ef941b3dbf5e9b0a094
thomasloven <thomasloven@gmail.com>
2013-02-05 01:28:03 +0100

    [tools] Automatic build and debug environment setup

---
+++ .gdbinit
@@ -0,0 +1,2 @@
+file kernel/kernel
+target remote localhost:1234
--- build/qemul.sh
+++ build/qemul.sh
@@ -3,8 +3,26 @@
 if tmux info | grep $TTY ; then
 	tmux split-window -h 'qemu-system-i386 -kernel kernel/kernel -curses -monitor telnet:localhost:4444,server -s -S'
 	tmux select-pane -L
+	tmux split-window -v 'i386-elf-gdb'
+	tmux select-pane -U
 	sleep 0.1
-	telnet localhost 4444
+	counter=10
+	until [[ $counter -lt 1 ]]; do
+		timestart=$(date +%s)
+		telnet localhost 4444
+		timeend=$(date +%s)
+		timediff=$(echo "$timeend - $timestart" | bc)
+		echo $timediff
+		if [[ $timediff -gt 1 ]]; then
+			echo "DONE!"
+			counter=0
+		else
+			echo "Go again"
+			counter=$counter-1
+			sleep 0.1
+		fi
+	done
+	tmux kill-pane -a
 else
 	qemu-system-i386 -kernel kernel/kernel -display curses -monitor stdio -s -S
 fi
