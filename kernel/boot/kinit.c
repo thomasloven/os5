5f36d61557e9f69046491f7a65f1bca35c14e8cc
Thomas Lovén <thomasloven@gmail.com>
2013-08-15 10:59:33 +0200

    Automatic rebuild of version number file on linking.

--- kernel/boot/kinit.c
+++ kernel/boot/kinit.c
@@ -68,8 +68,9 @@ registers_t *kinit(mboot_info_t *mboot, uint32_t mboot_magic)
 
   execve("/bin/init",0,0);
 
-  printf("Os5\n");
+  printf("Os5 by Thomas Lovén\n");
   printf("Kernel git data: [%s (%s)] %s\n", __kernel_git_hash, (__kernel_git_dirty)?"dirty":"clean", __kernel_git_date);
+  printf("%s: %s\n", __kernel_git_branch, __kernel_git_message);
   printf("Kernel compilation: %s %s", __kernel_build_date, __kernel_build_time);
   printf("\n");
 
--- kernel/boot/version.c
+++ kernel/boot/version.c
@@ -4,6 +4,8 @@
 char * __kernel_git_hash = GITHASH;
 char * __kernel_git_date = GITDATE;
 int __kernel_git_dirty = GITDIRTY;
+char * __kernel_git_message = GITMESSAGE;
+char * __kernel_git_branch = GITBRANCH;
 
 char * __kernel_build_date = __DATE__;
 char * __kernel_build_time = __TIME__;
