eb02ff4166d4644d09a3b54272286119a367efd9
Thomas Lovén <thomasloven@gmail.com>
2014-01-30 09:10:10 +0100

    Fix in register printout.

--- kernel/include/arch.h
+++ kernel/include/arch.h
@@ -35,11 +35,11 @@ uint32_t eflags, useresp, ss;
 } registers_t;
 
 #define print_registers(r) \
-  debug("\n\neax:%x ebx:%x ecx:%x edx:%x", (r)->eax, (r)->ebx, (r)->ecx, (r)->edx); \
-  debug("\nedi:%x esi:%x ebp:%x esp:%x", (r)->edi, (r)->esi, (r)->ebp, (r)->esp); \
-  debug("\neip:%x\ncs:%x ds:%x ss:%x", (r)->eip, (r)->cs, (r)->ds, (r)->ss); \
-  debug("\nuseresp:%x\neflags%x %b", (r)->useresp, (r)->eflags, (r)->eflags); \
-  debug("\nint_no:%x, err_code:%x %b", (r)->int_no, (r)->err_code, (r)->err_code);
+  debug("eax:%x ebx:%x ecx:%x edx:%x\n", (r)->eax, (r)->ebx, (r)->ecx, (r)->edx); \
+  debug("edi:%x esi:%x ebp:%x esp:%x\n", (r)->edi, (r)->esi, (r)->ebp, (r)->esp); \
+  debug("eip:%x\ncs:%x ds:%x ss:%x\n", (r)->eip, (r)->cs, (r)->ds, (r)->ss); \
+  debug("useresp:%x\neflags%x %b\n", (r)->useresp, (r)->eflags, (r)->eflags); \
+  debug("int_no:%x, err_code:%x %b\n", (r)->int_no, (r)->err_code, (r)->err_code);
 
 #endif
 
