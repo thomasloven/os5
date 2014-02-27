3fb774e727c3d1012b5a3b887110dcc2f7923906
Thomas Lovén <thomasloven@gmail.com>
2014-02-27 15:41:48 +0100

    A couple of virtual terminals.

--- tarfs/src/terminal_src/main.c
+++ tarfs/src/terminal_src/main.c
@@ -6,6 +6,17 @@
 #include <unistd.h>
 #include "keyboard.h"
 
+char **command;
+
+void switch_vterm(int num)
+{
+  if(num <0 || num > 4)
+    return;
+  if(!vterm[num])
+    terminal_init(num, 25, 80, command);
+  active_vterm = num;
+  copybuffer(vterm[num]);
+}
 
 int main(int argc, char **argv)
 {
@@ -15,19 +26,35 @@ int main(int argc, char **argv)
   screen_init();
   keyboard_init();
 
+  command = &argv[1];
+
   // Start with a single vterm
-  active_vterm = 0;
-  terminal_init(0, 25, 80, &argv[1]);
+  switch_vterm(0);
 
   while(1)
   {
-    char c;
+    char c, d;
     c = fgetc(stdin);
-    if((c = keyboard_decode(c)))
+    d = keyboard_decode(c);
+    if(kbd_ctrl)
     {
-      terminal_putch(vterm[active_vterm], c);
+      switch(d)
+      {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+          switch_vterm(d-'1');
+          continue;
+        default:
+          ;
+      }
+    }
+    if(d)
+    {
+      terminal_putch(vterm[active_vterm], d);
       copybuffer(vterm[active_vterm]);
-      write(vterm[active_vterm]->read_fd[1], &c, 1);
+      write(vterm[active_vterm]->read_fd[1], &d, 1);
     }
   }
 
